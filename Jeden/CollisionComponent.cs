d3125bf48f6e98779b5e8f11e355fdfa46f87fa5
ohbbies <jreeves2@crimson.ua.edu>
2014-04-14 03:09:58 -0500

    Collisions
    AABB collision detection, positions added to GameObject.

--- Jeden/CollisionComponent.cs
+++ Jeden/CollisionComponent.cs
@@ -6,13 +6,43 @@ using System.Threading.Tasks;
 
 namespace Jeden
 {
-    public class CollisionComponent
+    public class CollisionComponent : Component
     {
         public List<GameObject> CollidingGameObjects { get; set; }
 
+        public CollisionComponent(GameObject parent) : base(parent)
+        {
+
+        }
+
+        public void AddCollision(GameObject obj)
+        {
+            if (!CollidingGameObjects.Contains(obj))
+            {
+                CollidingGameObjects.Add(obj);
+            }
+        }
+
+        public void ClearCollisions()
+        {
+            CollidingGameObjects.Clear();
+        }
+
+        public void LoadContent(JedenGame game) 
+        {
+            
+        }
         public void Update(JedenGame game)
         {
 
+        }
+        public void Draw(JedenGame game) 
+        {
+
+        }
+        public void UnloadContent(JedenGame game) 
+        {
+            
         }
     }
 }
--- Jeden/CollisionManager.cs
+++ Jeden/CollisionManager.cs
@@ -8,6 +8,49 @@ namespace Jeden
 {
     public class CollisionManager
     {
-        //checks all registred collisioncomponents and adds the colliding game objects to the collisionComponent's list
+        List<CollisionComponent> CollisionComponents;
+
+        CollisionManager()
+        {
+            CollisionComponents = new List<CollisionComponent>();
+        }
+
+        public void Add(CollisionComponent cc)
+        {
+            if (!CollisionComponents.Contains(cc))
+            {
+                CollisionComponents.Add(cc);
+            }
+        }
+
+        public void Remove(CollisionComponent cc)
+        {
+            if (CollisionComponents.Contains(cc))
+            {
+                CollisionComponents.Remove(cc);
+            }
+        }
+
+        public void Detect()
+        {
+            for (int i = 0; i < CollisionComponents.Count; i++)
+            {
+                for (int j = i; j < CollisionComponents.Count; j++)
+                {
+                    GameObject o1 = CollisionComponents[i].Parent;
+                    GameObject o2 = CollisionComponents[j].Parent;
+                    if( !(o1.x+o1.width < o2.x ||
+                        o1.y < o2.y+o2.height ||
+                        o1.x > o2.x+o2.width ||
+                        o1.y + o1.height > o2.y))
+                    {
+                        //Register collision
+                        CollisionComponents[i].AddCollision(o2);
+                        CollisionComponents[j].AddCollision(o1);
+                    }
+                }
+            }
+        }
+
     }
 }
